f4dfad740db3cb569d2c6d81f9580ad340dcfc30
HP <HP@LAPTOP-8O0DF624>
2023-06-19 14:21:12 +0530

    Third commit to check git edit

--- src/main/java/com/demoAPIMain/GreetingController.java
+++ src/main/java/com/demoAPIMain/GreetingController.java
@@ -47,5 +47,10 @@ public class GreetingController {
 		return "Greeting with id: " +id+ " has been deleted"; 
 	}
 	
+<<<<<<< Upstream, based on origin/master
 	//git changes
+=======
+	//changes after git edit, to check weather this merge with git edit
+	
+>>>>>>> 2491ba7 Third commit to check git edit
 }
