383ddd5ef890a1038717ca2b3b06811a7bf2749f
HP <HP@LAPTOP-8O0DF624>
2023-06-19 14:18:10 +0530

    Second commit "more greeting service added"

--- src/main/java/com/demoAPIMain/GreetingController.java
+++ src/main/java/com/demoAPIMain/GreetingController.java
@@ -1,6 +1,11 @@
 package com.demoAPIMain;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,4 +24,27 @@ public class GreetingController {
 	 *  taskkill /PID <PID> /F        --> use this cmd to kill session and replace <PID> with above
 	 */
 
+	// code after first commit
+	
+	@GetMapping("/greet/{name}")
+	public String greetWithName(@PathVariable String name) {
+		return "Hello " +name+ "!";
+	}
+	
+	@PostMapping("/greet")
+	public String createGreeting(@RequestBody String name) {
+		return "Hello " +name+ "! created";
+	}
+	
+	@PutMapping("/greet/{id}")
+	public String updateGreeting(@PathVariable String id, @RequestBody String name) {
+		// Process the request to update the greeting with the provided ID
+		return "Hello " +name+ "! updated";
+	}
+	
+	@DeleteMapping("/greet/{id}")
+	public String deleteGreeting(@PathVariable String id) {
+		return "Greeting with id: " +id+ " has been deleted"; 
+	}
+	
 }
